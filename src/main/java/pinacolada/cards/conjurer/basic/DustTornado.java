d3aa6e036cebf6dd174254afd757a3d48b48bdc6
SevenDayCandle <persisting_delusions@yahoo.com>
2022-12-28 12:15:31 -0800

    - PlayCard fix

--- src/main/java/pinacolada/cards/conjurer/basic/DustTornado.java
+++ src/main/java/pinacolada/cards/conjurer/basic/DustTornado.java
@@ -3,6 +3,7 @@ package pinacolada.cards.conjurer.basic;
 
 import pinacolada.cards.base.*;
 import pinacolada.cards.base.fields.PCLCardTag;
+import pinacolada.powers.conjurer.PCLElementHelper;
 import pinacolada.resources.PCLEnum;
 import pinacolada.resources.conjurer.ConjurerResources;
 import pinacolada.skills.PCond;
@@ -24,5 +25,6 @@ public class DustTornado extends PCLCard
     public void setup(Object input)
     {
         addUseMove(PCond.exhaust(1), PMove.play(2, PCLCardTarget.SingleAlly, PCLCardGroupHelper.DiscardPile, PCLCardGroupHelper.Hand).setAlt(true).setCardTypes(PCLEnum.CardType.SUMMON));
+        addUseMove(PCond.onDiscard(), PMove.applyToEnemies(3, PCLElementHelper.Flowed, PCLElementHelper.Stoned));
     }
 }
