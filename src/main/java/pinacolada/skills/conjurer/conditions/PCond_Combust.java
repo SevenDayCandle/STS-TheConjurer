a9f6a461e59410085751ee9139a92c21b190c36a
SevenDayCandle <persisting_delusions@yahoo.com>
2023-01-12 18:03:02 -0800

    - Addition PSkillSaveData field for multiskills - Fix null skills

--- src/main/java/pinacolada/skills/conjurer/conditions/PCond_Combust.java
+++ src/main/java/pinacolada/skills/conjurer/conditions/PCond_Combust.java
@@ -7,27 +7,29 @@ import pinacolada.cards.base.AffinityReactions;
 import pinacolada.cards.base.PCLAffinity;
 import pinacolada.cards.base.PCLCardTarget;
 import pinacolada.cards.base.PCLUseInfo;
-import pinacolada.resources.PCLEnum;
-import pinacolada.resources.PGR;
 import pinacolada.resources.conjurer.ConjurerEnum;
 import pinacolada.resources.conjurer.ConjurerResources;
-import pinacolada.skills.*;
+import pinacolada.skills.PCond;
+import pinacolada.skills.PSkillData;
+import pinacolada.skills.PSkillSaveData;
+import pinacolada.skills.PTrigger;
+import pinacolada.skills.fields.PField_Affinity;
 
-public class PCond_Combust extends PCond
+public class PCond_Combust extends PCond<PField_Affinity>
 {
-
-    public static final PSkillData DATA = register(PCond_Combust.class, PField_Empty.class, 1, 1)
+    public static final PSkillData<PField_Affinity> DATA = register(PCond_Combust.class, PField_Affinity.class, 1, 1)
             .setColors(ConjurerEnum.Cards.THE_CONJURER)
             .selfTarget();
 
     public PCond_Combust()
     {
-        this((PCLAffinity) null);
+        super(DATA, PCLCardTarget.None, 0);
     }
 
     public PCond_Combust(PCLAffinity... affinities)
     {
-        super(DATA, PCLCardTarget.None, 0, affinities);
+        super(DATA, PCLCardTarget.None, 0);
+        fields.setAffinity(affinities);
     }
 
     public PCond_Combust(PSkillSaveData content)
@@ -35,28 +37,16 @@ public class PCond_Combust extends PCond
         super(content);
     }
 
-    public PCond_Combust(PSkill effect)
-    {
-        this();
-        setChild(effect);
-    }
-
-    public PCond_Combust(PSkill... effect)
-    {
-        this();
-        setChild(effect);
-    }
-
     @Override
     public boolean checkCondition(PCLUseInfo info, boolean isUsing, boolean fromTrigger)
     {
-        return alt ^ (affinities.isEmpty() ? info.reactions.hasCombust() : EUIUtils.all(affinities, info.reactions::hasCombust));
+        return fields.random ^ (fields.affinities.isEmpty() ? info.reactions.hasCombust() : EUIUtils.all(fields.affinities, info.reactions::hasCombust));
     }
 
     @Override
     public boolean triggerOnElementReact(AffinityReactions reactions, AbstractCreature target)
     {
-        if (affinities.isEmpty() ? reactions.hasCombust() : EUIUtils.all(affinities, reactions::hasCombust))
+        if (fields.affinities.isEmpty() ? reactions.hasCombust() : EUIUtils.all(fields.affinities, reactions::hasCombust))
         {
             if (this.childEffect != null)
             {
@@ -72,9 +62,9 @@ public class PCond_Combust extends PCond
     {
         if (hasParentType(PTrigger.class))
         {
-            return TEXT.conditions.wheneverYou(affinities.isEmpty() ? ConjurerResources.conjurer.tooltips.combust.title : EUIRM.strings.verbNoun(ConjurerResources.conjurer.tooltips.combust.title, getAffinityLevelOrString()));
+            return TEXT.conditions.wheneverYou(fields.affinities.isEmpty() ? ConjurerResources.conjurer.tooltips.combust.title : EUIRM.strings.verbNoun(ConjurerResources.conjurer.tooltips.combust.title, fields.getAffinityLevelOrString()));
         }
-        String base = affinities.isEmpty() ? ConjurerResources.conjurer.tooltips.combust.title : EUIRM.strings.adjNoun(getAffinityLevelOrString(), ConjurerResources.conjurer.tooltips.combust.title);
-        return alt ? TEXT.conditions.not(base) : base;
+        String base = fields.affinities.isEmpty() ? ConjurerResources.conjurer.tooltips.combust.title : EUIRM.strings.adjNoun(fields.getAffinityLevelOrString(), ConjurerResources.conjurer.tooltips.combust.title);
+        return fields.random ? TEXT.conditions.not(base) : base;
     }
 }
